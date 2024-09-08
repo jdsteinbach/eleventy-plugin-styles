16da94bc933bca68a8c93fe5f4a4bc5f2f5d2a9a
James Steinbach <jdsteinbach@gmail.com>
2024-09-07 21:25:11 -0600

    add'l changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = (eleventyConfig, globalOpts) => {
   globalOpts = globalOpts || {}
   eleventyConfig.namespace(globalOpts, () => {
+    console.log(globalOpts)
+    console.log(Object.keys(eleventyConfig))
+    eleventyConfig.addTemplateFormats('11ty.js')
     // Register `styles.11ty.js`
     // Get globalOpts into `styles.11ty.js`
   })
